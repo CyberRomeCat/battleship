3ac062211f986bfb24f51ade96b126462b914546
jenzie68 <jacecabalo@gmail.com>
2024-05-23 16:31:59 +0400

    feat: when hovering cells in board-0 of cell and following cells according to ship length

--- src/index.js
+++ src/index.js
@@ -37,10 +37,55 @@ const attackListener = () => {
   });
 };
 
+function changeColor(coordinate, direct, color) {
+  if (direct === "horizontal") {
+    for (let i = 0; i < q[0].length; i++) {
+      const selectCoord = document.querySelector(
+        `[data-coordinate=${coordinate}]`,
+      );
+      if (selectCoord === null) return;
+      if (selectCoord.style.backgroundColor === `lightblue`) {
+        return;
+      }
+      selectCoord.style.backgroundColor = `${color}`;
+
+      coordinate = coordinate.replace(
+        coordinate[1],
+        parseInt(coordinate[1], 10) + 1,
+      );
+    }
+  } else {
+    let charCode = coordinate[0].charCodeAt(0);
+    let tranlateToletter;
+    let coordinates;
+    let join = coordinate;
+    for (let i = 0; i < q[0].length; i++) {
+      const selectCoord = document.querySelector(`[data-coordinate=${join}]`);
+      if (selectCoord === null) return;
+      if (selectCoord.style.backgroundColor === `lightblue`) {
+        return;
+      }
+      selectCoord.style.backgroundColor = `${color}`;
+      charCode += 1;
+      tranlateToletter = String.fromCharCode(charCode);
+      coordinates = [tranlateToletter, coordinate[1], "-", coordinate[3]];
+      join = coordinates.join("");
+
+      coordinate = coordinate.replace(coordinate[0], tranlateToletter);
+    }
+  }
+}
+
 if (q.length !== 0) {
   allCells.forEach((c) => {
     const coord = c.getAttribute("data-coordinate");
     if (coord[3] === "0") {
+      c.addEventListener("mouseover", () =>
+        changeColor(coord, direction, "aquamarine"),
+      );
+      c.addEventListener("mouseout", () =>
+        changeColor(coord, direction, "bisque"),
+      );
       c.addEventListener("click", () => {
         player1.placeShip(q[0], coord[0], coord[1], direction, 1);
         if (direction === "horizontal") placeUserShip(coord, q[0]).horizontal();
